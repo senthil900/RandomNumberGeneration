81a863fb3e24210fc9e8e16dc138334586296893
senthil900 <110320976+senthil900@users.noreply.github.com>
2023-10-28 16:03:37 +0530

    Add files via upload

---
+++ RandomNumberGeneration.java
@@ -0,0 +1,10 @@
+package basictraining;
+import java.math.*;
+
+public class RandomNumberGeneration {
+   public static void main(String[] args) {
+	System.out.println("random number :"+(int)(Math.random()*100));
+	   
+	   
+}
+}
